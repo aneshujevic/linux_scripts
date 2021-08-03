4dd8ab0789841ab17a4076517dff995055c171fc
Anes Hujević <35279526+owlhoo@users.noreply.github.com>
2021-08-04 00:57:09 +0200

    add script for installing aur packages

---
+++ aur-install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [ $# -ne 1 ]; then
+    echo '[*] AUR package install script'
+    echo '[*] Usage: $0 <git clone url>'
+    exit -1
+fi
+
+DIR_NAME_AUR=$(echo $1 | grep -o -P '[(a-zA-Z0-9)]+(?=.git)')
+
+git clone $1 && cd $DIR_NAME_AUR && makepkg -si && cd - && (yes | rm -r $DIR_NAME_AUR)
